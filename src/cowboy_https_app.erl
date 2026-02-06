c238abf2448fbe674247122bfeb79c8ca6ce5b2d
jra-dev <almontejosc@gmail.com>
2026-02-06 11:10:55 +0800

    feature: bootup

--- src/cowboy_https_app.erl
+++ src/cowboy_https_app.erl
@@ -1,18 +1,25 @@
-%%%-------------------------------------------------------------------
-%% @doc cowboy_https public API
-%% @end
-%%%-------------------------------------------------------------------
-
 -module(cowboy_https_app).
 
 -behaviour(application).
 
 -export([start/2, stop/1]).
 
-start(_StartType, _StartArgs) ->
+start(_Type, _Args) ->
+    Dispatch = cowboy_router:compile([
+        {'_', [
+            {"/", toppage_h, []}
+        ]}
+    ]),
+    PrivDir = code:priv_dir(cowboy_https),
+    % erlang:display(PrivDir),
+    % CertFile = file:read_file(PrivDir ++ "/SSL_Certs/almondtea.crt"),
+    % erlang:display(CertFile),
+    {ok, _} = cowboy:start_tls(https, [
+        {port, 8443},
+        {certfile, PrivDir ++ "/SSL_Certs/almondtea.crt"},
+        {keyfile,  PrivDir ++ "/SSL_Certs/myCowboy.key"}
+    ], #{env => #{dispatch => Dispatch}}),
     cowboy_https_sup:start_link().
 
 stop(_State) ->
-    ok.
-
-%% internal functions
+    ok = cowboy:stop_listener(https).
